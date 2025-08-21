346e0a8d76347bc49abc9b4c50330a48b4ecdf55
MeBadDev <94725241+MeBadDev@users.noreply.github.com>
2025-08-21 09:16:33 +0800

    first commit

---
+++ src/client/java/net/mebdadev/mc_enchantments/SimpleEnchantmentsClient.java
@@ -0,0 +1,10 @@
+package net.mebdadev.mc_enchantments;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleEnchantmentsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
